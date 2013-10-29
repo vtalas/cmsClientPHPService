21e1cad9b8689ce732b7bea16cce46c2dbece256
ladeso <vladimir.talas@gmail.com>
2013-10-29 22:51:44 +0100

    skladani url

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
 //const BASE_URL = "http://localhost:62728/clientapi/d6907ffc-4e8e-4d62-96b3-306b8eafdbbf";
-const BASE_URL = "http://localhost:62728/clientapi/021724f3-90e5-4f97-ad6b-5b35114725b3";
+
+//jara tomka
+//const BASE_URL = "http://localhost:62728/clientapi/021724f3-90e5-4f97-ad6b-5b35114725b3";
+
+//pospa
+const BASE_URL = "http://localhost:62728/clientapi/e32765d7-c23c-4f1b-bd1e-26a8caa4cc97";
+
 //const BASE_URL = "http://ladeso.aspone.cz/clientapi/c78ee05e-1115-480b-9ab7-a3ab3c0f6643";
 ?>
